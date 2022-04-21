eab4b13232b640bbf7d70f04ebdde062b2631fae
Enes Aydin <71450603+Nochen0@users.noreply.github.com>
2022-04-21 20:36:11 +0300

    Replaced home page with library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,26 @@
 import { Box } from "@chakra-ui/layout"
-import { useSession } from "next-auth/react"
-import Head from "next/head"
 import { useEffect } from "react"
-import spotify from "../lib/SpotifyApi/spotify"
+import { useRouter } from "next/router"
+import Head from "next/head"
 
 const Home = () => {
-  const { data: session } = useSession()
+  const router = useRouter()
 
   useEffect(() => {
-    ;(async () => {
-      if (session?.accessToken) {
-        const featuredPlaylists = await spotify.getFeaturedPlaylists(
-          session.accessToken
-        )
-        const newReleases = await spotify.getNewReleases(session.accessToken)
-
-        const topArtists = await spotify.getUsersTopItems(
-          "artists",
-          session.accessToken
-        )
-      }
-    })()
-  }, [session])
+    router.replace("/collection/playlists")
+  }, [router])
 
   return (
     <>
       <Head>
-        <title>Home - Spotify</title>
+        <title>Your Library | Spotify</title>
       </Head>
       <Box
         bg="#121212"
         height="100%"
         width="calc(100vw - 260px)"
         overflowY="auto"
-      >
-        Home
-      </Box>
+      ></Box>
     </>
   )
 }
